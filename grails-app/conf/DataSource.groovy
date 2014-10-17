dcccab6350faddf0f216dfcd147e287e91dc1808
Ananya Behera <ananya_behera@outlook.com>
2014-10-17 15:15:30 +1100

    init

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,28 +1,33 @@
 dataSource {
     pooled = true
     driverClassName = "org.h2.Driver"
-	username = "sa"
-	password = ""
+	  username = "sa"
+	  password = ""
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
+
 // environment specific settings
 environments {
+    
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
+    
     test {
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
+    
     production {
         dataSource {
 
@@ -48,8 +53,8 @@ environments {
                testOnBorrow=true
                testWhileIdle=true
                testOnReturn=true
-               validationQuery="SELECT 1"*/
-            }
+               validationQuery="SELECT 1"
+            }*/
         }
     }
 }
