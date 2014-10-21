cb216b5ed6cc37b7e5f2ca4e2a0d119d99657ec8
Christian-Sesta <christian@Christians-MacBook-Pro.local>
2014-10-21 16:56:15 +1100

    Final test - All tables better appear now

--- grails-app/domain/cmsystem/Document.groovy
+++ grails-app/domain/cmsystem/Document.groovy
@@ -11,6 +11,7 @@ class Document {
     static constraints = {
 		name(blank:false,nullable:false)
 		file(nullable:false, maxSize:1073741824)
+        //fullPath(blank:false,nullable:false)
     }
 
     static mapping = {
