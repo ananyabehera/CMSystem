e75bb4cf1f438e770d79014cbecca1d0347e36d2
Christian-Sesta <christian@Christians-MacBook-Pro.local>
2014-10-21 16:52:02 +1100

    Anther test with document

--- grails-app/domain/cmsystem/Document.groovy
+++ grails-app/domain/cmsystem/Document.groovy
@@ -5,13 +5,12 @@ class Document {
 	String name
 	Date uploadDate = new Date()
 	String desc
-	//byte[] file
+	byte[] file
 	String type
 	
     static constraints = {
 		name(blank:false,nullable:false)
 		file(nullable:false, maxSize:1073741824)
-        //fullPath(blank:false,nullable:false)
     }
 
     static mapping = {
