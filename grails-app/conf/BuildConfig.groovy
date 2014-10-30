262148709321bfbd7556f64e07f00ee1537fde53
christian-sesta <christian.sesta@gmail.com>
2014-10-30 14:37:18 +1100

    Revert 4f6e312..7593468
    This rolls back to commit 4f6e31200720fc0f93a7cbe8776a93a3ed502267.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,14 +12,12 @@ grails.project.source.level = 1.6
 //]
 
 grails.project.dependency.resolution = {
-
-    pom true
     // inherit Grails' default dependencies
     inherits("global") {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
@@ -35,9 +33,9 @@ grails.project.dependency.resolution = {
 
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://snapshots.repository.codehaus.org"
-        /*mavenRepo "http://repository.codehaus.org"
-        mavenRepo "http://download.java.net/maven/2/"
-        mavenRepo "http://repository.jboss.com/maven2/"*/
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
@@ -47,25 +45,21 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ':hibernate:3.6.10.14'
-        //runtime ":jquery:1.11.0.2"
-        runtime ":resources:1.2.13"
-        compile ':asset-pipeline:1.8.3'
-        //compile ':scaffolding:2.1.0'
+        runtime ":hibernate:$grailsVersion"
+        runtime ":jquery:1.8.3"
+        runtime ":resources:1.1.6"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
 
-        build ':tomcat:7.0.52.1'
+        build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.4.0"
+        runtime ":database-migration:1.3.2"
 
-        compile ':cache:1.1.3'
-
-        //compile 'org.apache.commons:commons-lang3:3.1'
+        compile ':cache:1.0.1'
 		
-        //compile ':spring-security-core:2.0-RC4' - Requires grails 2.3
+		//compile ':spring-security-core:2.0-RC4' - Requires grails 2.3
     }
 }
