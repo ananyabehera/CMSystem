d72af93edfda46839bb7dedaae3fb99a4d98f176
Ananya Behera <ananya_behera@outlook.com>
2014-10-31 18:31:32 +1100

    Database reconfig

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -14,8 +14,6 @@ class BootStrap {
 			password: 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec',
 			permission: user).save(flush: true)
 
-		def newCatg = new Category(catgName: 'Skills', catgDesc: 'Expertise').save(flush: true)
-
 		def newCatg1 = new Category(catgName: 'Document Type', catgDesc: 'Different document types uploaded to the system').save(flush: true)
 		def newCatg2 = new Category(catgName: 'Skills', catgDesc: 'Expertise shown').save(flush: true)
 		def newCatg3 = new Category(catgName: 'Business Unit', catgDesc: ' The particular Line of Service').save(flush: true)
--- grails-app/domain/cmsystem/DocTag.groovy
+++ grails-app/domain/cmsystem/DocTag.groovy
@@ -2,6 +2,12 @@ package cmsystem
 
 class DocTag {
 
+	static belongsTo = [tag: Tag, document: Document]
+
     static constraints = {
+    }
+
+    static mapping = {
+    	table 'DocTag'
     }
 }
