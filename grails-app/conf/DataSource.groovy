0d575890d56fe03e293fb365c4f4639ac8c22a61
Christian-Sesta <Christian@Christians-MacBook-Pro.local>
2014-11-18 14:03:20 +1100

    Fix for Heroku v6.0

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,7 +30,7 @@ environments {
     
     production {
         dataSource {
-            dbCreate = "create-drop"
+            dbCreate = "create"
             driverClassName = "org.postgresql.Driver"
             dialect = org.hibernate.dialect.PostgreSQLDialect
 
@@ -39,21 +39,6 @@ environments {
             url = "jdbc:postgresql://"+uri.host+uri.path
             username = uri.userInfo.split(":")[0]
             password = uri.userInfo.split(":")[1]
-
-
-            /*dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }*/
         }
     }
 }
